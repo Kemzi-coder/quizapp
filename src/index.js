a35cefa159b64ecc1c37ab80737629d8246e4946
Kemzi <kemzi9585@gmail.com>
2021-07-21 12:44:14 +0300

    Final commit

--- src/index.js
+++ src/index.js
@@ -4,26 +4,30 @@ import 'regenerator-runtime/runtime'
 
 // Voting system variables
 
-const likesInput = document.querySelector('#likes-count')
-const dislikesInput = document.querySelector('#dislikes-count')
+const likeInput = document.querySelector('#likes-count')
+const dislikeInput = document.querySelector('#dislikes-count')
 const likeBtn = document.querySelector('#like-btn')
 const dislikeBtn = document.querySelector('#dislike-btn')
+
 // Likes and dislikes counters
+
 let likes = 0
 let dislikes = 0
 
 // Like button click listener
+
 likeBtn.addEventListener('click', () => {
    dislikeBtn.classList.remove('active')
    likeBtn.classList.add('active')
    likes++
-   likesInput.value = likes
+   likeInput.value = likes
 })
 
 // Dislike button click listener
+
 dislikeBtn.addEventListener('click', () => {
    likeBtn.classList.remove('active')
    dislikeBtn.classList.add('active')
    dislikes++
-   dislikesInput.value = dislikes
+   dislikeInput.value = dislikes
 })
